630f1db71f966630f9d6ffd5478273e95c85d731
YoonbeomSo <ybum1224@naver.com>
2023-01-08 22:24:43 +0900

    :zap: 스프링 컨테이너와 스프링 빈 조회 study

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicInfoTest.java
@@ -0,0 +1,53 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextBasicInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조화")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        //구체타입으로 조회하면 유연성이 떨어진다.
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanNameX() {
+        //실패 테스트 진행
+//        MemberService xxxx = ac.getBean("XXXX", MemberService.class);  //예외 발생 로직
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("XXXX", MemberService.class));
+    }
+
+
+
+}
