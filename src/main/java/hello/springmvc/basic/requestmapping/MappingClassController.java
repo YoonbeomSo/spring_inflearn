663b7d49a0f971fbeca7dff5f13d54fa9d9ff40d
YoonbeomSo <ybum1224@naver.com>
2023-02-07 22:15:45 +0900

    :rocket: 요청 매핑 - API 예시

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/mapping/users")
 public class MappingClassController {
 
+    /*
+        회원 관리 API
+        회원 목록 조회: GET
+        회원 등록: POST
+        회원 조회: GET
+        회원수정: PATCH /users/{userId} 회원 삭제: DELETE /users/{userId}
+    */
     @GetMapping()
     public String user() {
         return "get users";
