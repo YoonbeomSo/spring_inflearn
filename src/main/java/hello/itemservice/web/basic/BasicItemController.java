a335e09fd1fb8b2fe97a68c2add2a5c025eac95a
YoonbeomSo <ybum1224@naver.com>
2023-03-03 22:16:04 +0900

    :sunny: 웹 페이지 만들기 - 컨틀롤러 Get/Post Mapping 복습

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -55,7 +55,8 @@ public class BasicItemController {
     }
 
     /**
-     * 등록 로직
+     * 등록 로직1
+     * @RequestParam 을 사용해 데이터를 받는다.
      */
 //    @PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
@@ -72,6 +73,10 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    /**
+     * 등록 로직2
+     * @ModelAttribute 를 사용해 dto로 데이터를 받는다.
+     */
 //    @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item) {
         itemRepository.save(item);
@@ -79,27 +84,47 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    /**
+     * 등록 로직3
+     * @ModelAttribute 를 사용해 dto로 데이터를 받는다.
+     * parmeter 의 첫글자를 소문자로 변경하면 이름 지정을 생략하여 사용가능하다.
+     */
 //    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item) { //첫글자 소문자로 변경하면 default로 이름지정 생략하여 사용가능
+    public String addItemV3(@ModelAttribute Item item) {
         itemRepository.save(item);
         return "basic/item";
     }
 
+    /**
+     * 등록 로직4
+     * ModelAttribute 는 생략 가능하다.
+     */
 //    @PostMapping("/add")
     public String addItemV4(Item item) { //ModelAttribute 생략 가능
         itemRepository.save(item);
         return "basic/item";
     }
 
-    /* PRG(PostRedirectGet) 방식 적용 */
+    /**
+     * 등록 로직5
+     * post 요청 후 화면단에서 새로 고침을 하면 마지막에 사용된 post 요청이 계속해서 전송된다. -> post buiseness 로직 중복 수행
+     * 이를 해결하기 위해 post 요청 후 redirect로 get 요청을 다시 수행하게 되면,
+     * 새로 고침을 해도 마지막에 호출된 get 요청이 전송되어 문제가 해결된다.
+     * 이러한 방법을 PRG(PostRedirectGet)이라고 한다.
+     */
 //    @PostMapping("/add")
     public String addItemV5(Item item) { //ModelAttribute 생략 가능
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
     }
 
+    /**
+     * 등록 로직6
+     * PRG 방식에서 RedirectAttributes를 사용하면 ModelAttribute와 같이
+     * 화면 단으로 모델을 넘길 수 있다.
+     */
     @PostMapping("/add")
-    public String addItemV5(Item item, RedirectAttributes redirectAttributes) { //ModelAttribute 생략 가능
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) { //ModelAttribute 생략 가능
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -120,6 +145,7 @@ public class BasicItemController {
 
     /**
      * 수정 로직
+     * redirect 에서도 pathVariable 형태를 사용할 수 있다.
      */
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
