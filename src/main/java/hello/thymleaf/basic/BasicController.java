c90049ed9b8587011a6ff9123090e40cb2706ddb
YoonbeomSo <ybum1224@naver.com>
2023-03-12 22:22:21 +0900

    FEAT : 리터럴(literal)

--- src/main/java/hello/thymleaf/basic/BasicController.java
+++ src/main/java/hello/thymleaf/basic/BasicController.java
@@ -79,6 +79,13 @@ public class BasicController {
     }
 
 
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "spring!");
+        return "basic/literal";
+    }
+
+
     @Data
     static class User {
         private String username;
