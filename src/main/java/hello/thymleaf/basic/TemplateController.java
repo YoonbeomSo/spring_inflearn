ebd1df77e29c3e0b3a0c41214859f33336aed4da
YoonbeomSo <ybum1224@naver.com>
2023-03-19 22:30:24 +0900

    FEAT : 템플릿 레이아웃1l

--- src/main/java/hello/thymleaf/basic/TemplateController.java
+++ src/main/java/hello/thymleaf/basic/TemplateController.java
@@ -12,4 +12,12 @@ public class TemplateController {
     public String template() {
         return "template/fragment/fragmentMain";
     }
+
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+
 }
