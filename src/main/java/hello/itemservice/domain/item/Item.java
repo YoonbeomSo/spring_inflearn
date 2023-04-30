217ca6ca6d2472f3329e0a7b2c5f5bc9d41b3643
YoonbeomSo <ybum1224@naver.com>
2023-04-30 15:01:27 +0900

    @Bean Validation groups 를 사용한 Validator 분 -> 복잡도가 너무 올라감, 폼객체분리를 통해 해결하는게 좋음

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -13,16 +13,16 @@ import javax.validation.constraints.NotNull;
 //but, jdk14 이후 버전부터는 javascript가 지원되지 않는 GraalVM 을 사용 -> @ScriptAssert 는 현재 사용할 수 없는 기능이다.
 public class Item {
 
-    @NotNull //수정 요구사항 추가
+    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
     private Long id;
-    @NotBlank(message = "공백x")
+    @NotBlank(message = "공백x", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price; //A -> TypeMismatch
 
-    @NotNull
-    //@Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
