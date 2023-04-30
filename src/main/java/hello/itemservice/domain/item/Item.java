162310f281252c64ae772e2d7f9b3ea4182ce1ef
YoonbeomSo <ybum1224@naver.com>
2023-04-30 14:45:42 +0900

    @Bean Validation 한계

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,9 +10,10 @@ import javax.validation.constraints.NotNull;
 
 @Data
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")//Object Error 처리방법
-//but, jdk14 이후 버전부터는 javascript가 지원되지 않는 GraalVM 을 사용 ->@ScriptAssert 는 현재 사용할 수 없는 기능이다.
+//but, jdk14 이후 버전부터는 javascript가 지원되지 않는 GraalVM 을 사용 -> @ScriptAssert 는 현재 사용할 수 없는 기능이다.
 public class Item {
 
+    @NotNull //수정 요구사항 추가
     private Long id;
     @NotBlank(message = "공백x")
     private String itemName;
@@ -21,7 +22,7 @@ public class Item {
     private Integer price; //A -> TypeMismatch
 
     @NotNull
-    @Max(9999)
+    //@Max(9999)
     private Integer quantity;
 
     public Item() {
