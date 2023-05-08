a419eeb7e9c1a2031129c9b1c76b56641653e707
YoonbeomSo <ybum1224@naver.com>
2023-05-08 20:59:46 +0900

    FEAT : add login service(Used Cookie)

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -23,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("login")
-    public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -34,9 +38,25 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        //로그인 성공 처리 TODO
+        //로그인 성공 처리
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie isCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(isCookie);
 
         return "redirect:/";
 
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
