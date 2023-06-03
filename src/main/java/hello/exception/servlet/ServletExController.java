1acf0f067b9ab1fac70c0b24f1ffe9ce75a67161
YoonbeomSo <ybum1224@naver.com>
2023-06-03 10:53:39 +0900

    FEAT : ServletException - error screen

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
+
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
