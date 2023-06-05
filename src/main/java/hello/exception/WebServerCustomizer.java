e890ac415867c3e92628131f345382c51e1b852f
YoonbeomSo <ybum1224@naver.com>
2023-06-05 19:45:43 +0900

    FEAT : ServletException - error page Operating principle

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -8,6 +8,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    /**
+     * <예외 발생과 오류 페이지 요청 흐름>
+     * 1. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+     * 2. WAS `/error-page/500` 다시 요청 -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러(/error- page/500) -> View
+     */
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
