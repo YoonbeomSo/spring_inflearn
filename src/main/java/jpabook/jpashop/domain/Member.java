4817e5a876f98f353fa779ddef3a82c4285957e8
YoonbeomSo <ybum1224@naver.com>
2023-09-30 22:16:39 +0900

    회원 조회 API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,12 +18,12 @@ public class Member {
     @Column(name="member_id")
     private Long id;
 
-
     private String name;
 
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = "member") //order table 에 있는 "member" fild 에 의해 mapping 된것이라는 뜻 -> 연관 관계의 주인이 아닌 반대 쪽에 적어준다.
+//    @JsonIgnore
+    @OneToMany(mappedBy = "member") //order table 에 있는 "member" fild 에 의해 mapping 된 것 이라는 뜻 -> 연관 관계의 주인이 아닌 반대 쪽에 적어 준다.
     private List<Order> orders = new ArrayList<>(); //collection은 필드에서 바로 초기해주는 것이 좋다.
 }
