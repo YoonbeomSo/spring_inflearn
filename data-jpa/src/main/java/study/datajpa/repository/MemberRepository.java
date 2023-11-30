30a228d966337bcbd5fae92284ae8d5972f23dc2
YoonbeomSo <ybum1224@naver.com>
2023-11-30 21:28:30 +0900

    feat: @Query, 값, DTO 조회하기

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -14,9 +15,16 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findTop3HelloBy();
 
     //    @Query(name = "Member.findByUsername") //생략 가능
-    List<Member> findByUsername(@Param("username") String username); //namedQuery 에서 @Param 필수
+    List<Member> findByUsername(@Param("username") String username); //namedQuery 에서 @Param 필기
 
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    //jpql 이 지원하는 new operation 문법 사용
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 
 }
