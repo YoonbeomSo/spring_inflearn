ecd454a88c0f628ee0d48ca09a32209a626c9ccd
YoonbeomSo <ybum1224@naver.com>
2024-01-23 07:52:24 +0900

    feat: 새로운 엔티티를 구별하는 방법(save -> persist or merge)

---
+++ data-jpa/src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,48 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String>  {
+
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+
+    @Id
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    //GeneratedValue 가 아닌 save 호출시, isNew() 함수에서 merge 가 동작하여 select query 가 발생하느 문제점 해결 방안.
+    //-> Persistable<?> 인터페이스를 상속받아 isNew() 함수를 Override 하여 구현한다.
+    @Override
+    public boolean isNew() {
+//        return false;
+        return createdDate == null; //등록기간을 조합해서 구현하면 효율적이다. -> 아마 uuid 를 사용한 id 생성전략에서 사용할 수 있을것 같다.
+    }
+}
